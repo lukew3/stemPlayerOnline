e933b0497a3d82e083f865c67a393ec0ff73cfb2
lukew3 <lukew25073@gmail.com>
2022-02-25 00:12:14 -0500

    add one more time

--- data.js
+++ data.js
@@ -5,6 +5,12 @@ const songs = {
 		"3": "https://lukew3.com/recolorbot/photos/Donda_2_We%20Did%20It%20Kid_3.mp3",
 		"4": "https://lukew3.com/recolorbot/photos/Donda_2_We%20Did%20It%20Kid_4.mp3"
 	},
+	"one_more_time": {
+		"1": "https://lukew3.com/recolorbot/photos/One-More-Time_vocals.mp3",
+		"2": "https://lukew3.com/recolorbot/photos/One-More-Time_drums.mp3",
+		"3": "https://lukew3.com/recolorbot/photos/One-More-Time_bass.mp3",
+		"4": "https://lukew3.com/recolorbot/photos/One-More-Time_other.mp3",
+	},
 	"fuck_you": {
 		"1": "https://storage.googleapis.com/stem-share-demucs-output/fuck-you-5a6f3d/vocals.mp3", //vocals
 		"2": "https://storage.googleapis.com/stem-share-demucs-output/fuck-you-5a6f3d/drums.mp3", //drums
