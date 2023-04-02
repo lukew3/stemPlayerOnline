ef34969c20c2197ea00a7a3e59c3820bdd8499bb
lukew3 <lukew25073@gmail.com>
2023-04-02 05:58:20 -0400

    update sw for offline

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Service worker adapted from: https://developers.google.com/web/fundamentals/primers/service-workers
-var CACHE_NAME = 'stemplayeronline-beta-cache-v1';
-var urlsToCache = [
+const CACHE_NAME = 'stemplayeronline-beta-cache-v2';
+const urlsToCache = [
 	'/',
 	'/main.css',
 	'/js/app.js',
@@ -13,50 +13,46 @@ var urlsToCache = [
 	'/img/collapseArrow.svg',
 	'/img/discordIcon.svg',
 	'/img/folder.svg',
-	'/img/logo-512.png'
+	'/img/logo-512.png',
+	'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap',
+	'https://fonts.gstatic.com/s/roboto/v30/KFOmCnqEu92Fr1Mu72xKOzY.woff2',
+	'https://fonts.gstatic.com/s/roboto/v30/KFOmCnqEu92Fr1Mu4mxK.woff2',
+	'https://fonts.gstatic.com/s/roboto/v30/KFOlCnqEu92Fr1MmEU9fCRc4EsA.woff2',
+	'https://fonts.gstatic.com/s/roboto/v30/KFOlCnqEu92Fr1MmEU9fBBc4.woff2',
+	'https://unpkg.com/web-audio-daw'
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
+    // Cache urlsToCache
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-        if ('navigationPreload' in self.registration) {
-          await self.registration.navigationPreload.enable();
-        }
-  })());
-  // Delete old caches
   event.waitUntil(
-    caches.keys().then((keyList) => {
+    // Delete old caches
+    caches.keys().then((keys) => {
       return Promise.all(
-        keyList.map((key) => {
-          if (key === CACHE_NAME) return;
-          return caches.delete(key);
-        })
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete())
       );
     })
-  );
-  self.clients.claim();
-});                                                                                                     
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+    /*
+    // Nav preload
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+    */
+  )
+  // self.clients.claim();
 });
 
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cacheRes) => {
+      return cacheRes || fetch(event.request);
+    });
+  );
+});
