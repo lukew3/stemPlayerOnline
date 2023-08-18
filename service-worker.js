d88dc6ebc0344bf7d8a4f3197d4a5bb8269f6694
lukew3 <lukew25073@gmail.com>
2023-08-18 16:49:42 +0000

    update cache version

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service worker adapted from: https://developers.google.com/web/fundamentals/primers/service-workers
-const CACHE_NAME = 'stemplayeronline-beta-cache-v2';
+const CACHE_NAME = 'stemplayeronline-beta-cache-v5';
 const urlsToCache = [
 	'/',
 	'/main.css',
