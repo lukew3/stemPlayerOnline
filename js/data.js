fef4cee182726bdbd0b4d4591246a9e14f2d828e
lukew3 <lukew25073@gmail.com>
2025-02-21 16:21:32 +0000

    change beta url

--- js/data.js
+++ js/data.js
@@ -1,333 +1,303 @@
-const playlist = [
+let playlist = [
 	{
 		title: "Donda Chant",
 		bpm: 103,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1pPquYD7GVYEaU9xYq-QvEcZgj4Db2InT",
-			"https://drive.google.com/uc?export=download&id=1pP-XLPFAtu6YpXyX4HmY0wgtoNNRWihi",
-			"https://drive.google.com/uc?export=download&id=1pKPkq5iWm_iPUMilshOHCUQcNno6lNA0",
-			"https://drive.google.com/uc?export=download&id=1pNhUZoLSUnjBklz-pyaaZjRKQD0-2j8D"
+			"/songs/Donda-Chant/1.flac",
+			"/songs/Donda-Chant/2.flac",
+			"/songs/Donda-Chant/3.flac",
+			"/songs/Donda-Chant/4.flac",
 		],
 	},
 	{
 		title: "Hurricane",
 		bpm: 80,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1i2fpw9J8Brn-Qs5p0UBYAkW371Kts5PL",
-			"https://drive.google.com/uc?export=download&id=1hr0shWgouaoImysDTH6vmj2nDcBuL2RW",
-			"https://drive.google.com/uc?export=download&id=1i287AZc0EuLxJDh8HDVsNcLT4ekn0OI3",
-			"https://drive.google.com/uc?export=download&id=1iDCX9l-CIZ4ARF7f6Vhk9uv0cL5JcxXJ"
+			"/songs/Hurricane/1.flac",
+			"/songs/Hurricane/2.flac",
+			"/songs/Hurricane/3.flac",
+			"/songs/Hurricane/4.flac",
 		],
 	},
 	{
 		title: "Moon",
 		bpm: 162, //66?
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1kY-Ap1fpRPXGO5Mhx1O0bOuGc53RajFe",
-			"https://drive.google.com/uc?export=download&id=1kHfjNMeJ4V6DEFFNH-rA8FAqVIkwTjrR",
-			"https://drive.google.com/uc?export=download&id=1kPGIU_Zq1docN_VZtCD0QIt2Q3iL8mYx",
-			"https://drive.google.com/uc?export=download&id=1kP7e-G3ezQxRBFQJb5EcJStmzH4VqIzQ"
+			"/songs/Moon/1.flac",
+			"/songs/Moon/2.flac",
+			"/songs/Moon/3.flac",
+			"/songs/Moon/4.flac",
 		],
 	},
 	{
 		title: "Life of the Party",
 		bpm: 79,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1j8lh_dDC3mE66B3nIEHpU3se7lJujQsB",
-			"https://drive.google.com/uc?export=download&id=1jD3rReo6i0oy5hDNFoG_24y5SNFMtTPf",
-			"https://drive.google.com/uc?export=download&id=1jInVzxoYyIhLrwHkEMge8FoMEO8T-FAQ",
-			"https://drive.google.com/uc?export=download&id=1jDuC6zwD1E8xVp7m1J3kXLpTqevnUoHJ"
+			"/songs/Life-Of-The-Party/1.flac",
+			"/songs/Life-Of-The-Party/2.flac",
+			"/songs/Life-Of-The-Party/3.flac",
+			"/songs/Life-Of-The-Party/4.flac",
 		],
 	},
 	{
 		title: "Off the Grid",
 		bpm: 138,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1s8WXXS4R5VtX2JnNBBZTckMXqJpGx9Jn",
-			"https://drive.google.com/uc?export=download&id=1sK62NVvesD0dDpMa-srLR0T9d7GA01Zr",
-			"https://drive.google.com/uc?export=download&id=1sJm7Gokl4sWIRKEdsRwoIWwF59gh7-4E",
-			"https://drive.google.com/uc?export=download&id=1sI8HXO5pP36hx2sJZ_FmQMpkrW5xXWff"
+			"/songs/Off-The-Grid/1.flac",
+			"/songs/Off-The-Grid/2.flac",
+			"/songs/Off-The-Grid/3.flac",
+			"/songs/Off-The-Grid/4.flac",
 		],
 	},
 	{
 		title: "Jail",
 		bpm: 110,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1qOwlIg-u7RiZle-__qw0VqzMeiYQt_Du",
-			"https://drive.google.com/uc?export=download&id=1q5TjC5julN4GTk5rWk7470dmmkeC3RvM",
-			"https://drive.google.com/uc?export=download&id=1q5uigBD7MppD3YpqZler6Pn7dQaftSv2",
-			"https://drive.google.com/uc?export=download&id=1qXQcDotzaZGxz9UBu14faFwntRG38lpY"
+			"/songs/Jail/1.flac",
+			"/songs/Jail/2.flac",
+			"/songs/Jail/3.flac",
+			"/songs/Jail/4.flac",
 		],
 	},
 	{
 		title: "Praise God",
 		bpm: 118,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1s7jhezCHA_kL6m6r4fJYq0wT37IdhG6A",
-			"https://drive.google.com/uc?export=download&id=1s3-a-5RMxBfCfZy4XhYcG6OgfaMoM-DZ",
-			"https://drive.google.com/uc?export=download&id=1rjZgQXTCQKbEaBy2AMgFbtOsqx_DYSCC",
-			"https://drive.google.com/uc?export=download&id=1s8FG15hRxcPtPtyKTefHF8ayXu1GqBZE"
+			"/songs/Praise-God/1.flac",
+			"/songs/Praise-God/2.flac",
+			"/songs/Praise-God/3.flac",
+			"/songs/Praise-God/4.flac",
 		],
 	},
 	{
 		title: "Come To Life",
 		bpm: 127,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1lXl0Vo-S8E0SenpxuDyIYjMVLFZt6HB5",
-			"https://drive.google.com/uc?export=download&id=1lMR-NyjPxjJcxE4dcLAXXgRjkxuxxQHg",
-			"https://drive.google.com/uc?export=download&id=1lOOMaE5yGkV0dbymEGA7D1oRxgDo530l",
-			"https://drive.google.com/uc?export=download&id=1l_5ucjV2InoYUNxRPsgn8WYce9RAWACZ"
+			"/songs/Come-To-Life/1.flac",
+			"/songs/Come-To-Life/2.flac",
+			"/songs/Come-To-Life/3.flac",
+			"/songs/Come-To-Life/4.flac",
 		],
 	},
 	{
 		title: "Believe What I Say",
 		bpm: 100,
 		tracks:	[
-			"https://drive.google.com/uc?export=download&id=1iRb_QhgMn4IfgH5ts4X7SBuv90Gr3EDv",
-			"https://drive.google.com/uc?export=download&id=1iQPx-ahnx7SxvfQKBS5CdeRj3r5e78Cc",
-			"https://drive.google.com/uc?export=download&id=1idS_xdkOuoflT33YuIhRBNKFhj_NqSYz",
-			"https://drive.google.com/uc?export=download&id=1ij2qomZbsK8KxbRBcr2Qax_1G8t4BI3k"
+			"/songs/Believe-What-I-Say/1.flac",
+			"/songs/Believe-What-I-Say/2.flac",
+			"/songs/Believe-What-I-Say/3.flac",
+			"/songs/Believe-What-I-Say/4.flac",
 		],
 	},
 	{
 		title: "No Child Left Behind",
 		bpm: 135,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1nzZhgFaur0NcQ5T4VIy4wh-jpf0CMtlW",
-			"https://drive.google.com/uc?export=download&id=1oMLPgCR8r1Jih2YHuxaJqnEaAU5Q4gwD",
-			"https://drive.google.com/uc?export=download&id=1oMYvOQm4GuFEq05ZEr-iILgqY6G8Fan4",
-			"https://drive.google.com/uc?export=download&id=1oI6KRe7mPTbo_hS8rGAnMrpgK5Hap7Gk"
+			"/songs/No-Child-Left-Behind/1.flac",
+			"/songs/No-Child-Left-Behind/2.flac",
+			"/songs/No-Child-Left-Behind/3.flac",
+			"/songs/No-Child-Left-Behind/4.flac",
 		],
 	},
 	{
 		title: "Up From The Ashes",
 		bpm: 143,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1lHStyeNsYtYebBbAhuHgs0P8w6cpG5yy",
-			"https://drive.google.com/uc?export=download&id=1lEvpyyv28YWoPf-HVsiS_43Ukpb6N0bm",
-			"https://drive.google.com/uc?export=download&id=1lLwD4h29iPvwVRrlLGj8uDk65uZSG6Mr",
-			"https://drive.google.com/uc?export=download&id=1lUHqYItLIhPCHPk1iJoqbTyUOmNVowNp"
+			"/songs/Up-From-The-Ashes/1.flac",
+			"/songs/Up-From-The-Ashes/2.flac",
+			"/songs/Up-From-The-Ashes/3.flac",
+			"/songs/Up-From-The-Ashes/4.flac",
 		],
 	},
 	{
 		title: "God Breathed",
 		bpm: 93,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1nxfTqbuQ0Zcx-N69BoZHEuQWNi06wJo6",
-			"https://drive.google.com/uc?export=download&id=1nvb0U8LRdFX_KGbVdSx64czQ5XjlUDlo",
-			"https://drive.google.com/uc?export=download&id=1nwIVZJPh9c3IWCm2jg-22EKH2R3AU9Yk",
-			"https://drive.google.com/uc?export=download&id=1nyYGLvJAkGtr_1GeSUh4keTRDEgOsgnK"
+			"/songs/God-Breathed/1.flac",
+			"/songs/God-Breathed/2.flac",
+			"/songs/God-Breathed/3.flac",
+			"/songs/God-Breathed/4.flac",
 		],
 	},
 	{
 		title: "Lord I Need You",
 		bpm: 83,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1pVIa0xWn8aWxsVxhc28rFDkicStUBU5S",
-			"https://drive.google.com/uc?export=download&id=1pnnFwCvGTw7LtDwFW8oGe7ZnsNKBtet_",
-			"https://drive.google.com/uc?export=download&id=1pnFDw-0rXZLWtvFuoIF6NDaisvcEtU8a",
-			"https://drive.google.com/uc?export=download&id=1pi-EYaNW7gyiZ933dC3zxjNtR3iw8DTN"
+			"/songs/Lord-I-Need-You/1.flac",
+			"/songs/Lord-I-Need-You/2.flac",
+			"/songs/Lord-I-Need-You/3.flac",
+			"/songs/Lord-I-Need-You/4.flac",
 		],
 	},
 	{
 		title: "24",
 		bpm: 110,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1l0NpW2-YUZyYsjz1biYEFvIbQUEBX3QT",
-			"https://drive.google.com/uc?export=download&id=1kyiCYl8UYDm4VJnzg3-gdQdWWIF-FFOB",
-			"https://drive.google.com/uc?export=download&id=1l3FPdBLqpYHPrrwDCxM2-Bz0tUSgZ97L",
-			"https://drive.google.com/uc?export=download&id=1l-ErwbgSsSmvwBh6lueCc7QE1l7QWnba"
+			"/songs/24/1.flac",
+			"/songs/24/2.flac",
+			"/songs/24/3.flac",
+			"/songs/24/4.flac",
 		]
 	},
 	{
 		title: "Junya",
 		bpm: 148,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1q09C2bBSzvA8mwm1ltaUbD-45EKGgLXE",
-			"https://drive.google.com/uc?export=download&id=1pw-0KC2ZXSS90XYxzenHJ6Js8LUo1T5O",
-			"https://drive.google.com/uc?export=download&id=1q9enLeSvOZNBAjo30W1n09J1wCQVIV-e",
-			"https://drive.google.com/uc?export=download&id=1pyeez4lKiMYKx2jPjUIJTJAYDBVB-XMF"
+			"/songs/Junya/1.flac",
+			"/songs/Junya/2.flac",
+			"/songs/Junya/3.flac",
+			"/songs/Junya/4.flac",
 		],
 	},
 	{
 		title: "Never Abandon Your Family",
 		bpm: 90,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1nuRC25F8glgtTQwSEE3WAJcbXDOuquFd",
-			"https://drive.google.com/uc?export=download&id=1nX4N5g99xCSR49GPcH7BEIjVkE3n-9ko",
-			"https://drive.google.com/uc?export=download&id=1nt9PzBRvtCTpQO8A6Q8Dpt7OcJThs4eg",
-			"https://drive.google.com/uc?export=download&id=1n_ev_2U3ips4KwsIzcrGWKNrVG06Mp7c"
+			"/songs/Never-Abandon-Your-Family/1.flac",
+			"/songs/Never-Abandon-Your-Family/2.flac",
+			"/songs/Never-Abandon-Your-Family/3.flac",
+			"/songs/Never-Abandon-Your-Family/4.flac",
 		],
 	},
 	{
 		title: "Donda",
 		bpm: 106,
 		tracks:	[
-			"https://drive.google.com/uc?export=download&id=1rlOtRPMTlUMBcs7nuGyn-pd83cAcEtvI",
-			"https://drive.google.com/uc?export=download&id=1roYJ_a4EUMXrTLGRmSkC7i6SBMxqzZBe",
-			"https://drive.google.com/uc?export=download&id=1ru9O24fZdguL_nCbcngb3WmKHDZnVD-d",
-			"https://drive.google.com/uc?export=download&id=1rmjRIs6T8HkVnhcw6PQRu2ToQdchkbrT"
+			"/songs/Donda/1.flac",
+			"/songs/Donda/2.flac",
+			"/songs/Donda/3.flac",
+			"/songs/Donda/4.flac",
 		],
 	},
 	{
 		title: "Keep My Spirit Alive",
 		bpm: 166,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1kwAhSThL146szXIFCWjHoT9Zawmdy-H9",
-			"https://drive.google.com/uc?export=download&id=1l0UfiLYOPEZ4Ga4tjpB93txeMiXOM3S2",
-			"https://drive.google.com/uc?export=download&id=1l1rfvQkuLbQR5Y4eL8d4DhGTfLPlXYkK",
-			"https://drive.google.com/uc?export=download&id=1kodUEFe97Vg0T-l7w0ohCjON5NsRjLGT"
+			"/songs/Keep-My-Spirit-Alive/1.flac",
+			"/songs/Keep-My-Spirit-Alive/2.flac",
+			"/songs/Keep-My-Spirit-Alive/3.flac",
+			"/songs/Keep-My-Spirit-Alive/4.flac",
 		],
 	},
 	{
 		title: "Jesus Lord pt. 2",
 		bpm: 107,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1qjNHfaF-ltstzxQJPFJd7Ct5LOtp3e57",
-			"https://drive.google.com/uc?export=download&id=1qqolMSh0iili2SxmRKoLnPFlx_Nb6yWG",
-			"https://drive.google.com/uc?export=download&id=1qoYXHj6ynZnORAUfX7UQrxJq1YLwccrh",
-			"https://drive.google.com/uc?export=download&id=1qnv8wZiGpdcU-kpacJ1GfFvSuPmKPBSR"
+			"/songs/Jesus-Lord-2/1.flac",
+			"/songs/Jesus-Lord-2/2.flac",
+			"/songs/Jesus-Lord-2/3.flac",
+			"/songs/Jesus-Lord-2/4.flac",
 		],
 	},
 	{
 		title: "Heaven and Hell",
 		bpm: 83,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1pI8esRhlZozIv7xXl0m3wA1quCwAZsBF",
-			"https://drive.google.com/uc?export=download&id=1pBcuJzpv0tID0l8S6KUYijchA6sEgOKE",
-			"https://drive.google.com/uc?export=download&id=1ph5A0yPcC3bZlDYpuRTX5wBO14bJIp2Q",
-			"https://drive.google.com/uc?export=download&id=1ph7vqPz4b6GrIGMIJP94vz3gCVuAKMqD"
+			"/songs/Heaven-And-Hell/1.flac",
+			"/songs/Heaven-And-Hell/2.flac",
+			"/songs/Heaven-And-Hell/3.flac",
+			"/songs/Heaven-And-Hell/4.flac",
 		],
 	},
 	{
 		title: "Remote Control",
 		bpm: 110,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1kgLiW--6Vq4aqcDSyDyjnS6bE52JSRQP",
-			"https://drive.google.com/uc?export=download&id=1k_DXDAfZUZ9B9Fxqi_X9_ok1V6xLjxH8",
-			"https://drive.google.com/uc?export=download&id=1kbSCKFR6cg1HT_T3X44ZlQehUZksnWme",
-			"https://drive.google.com/uc?export=download&id=1kltsRcLTeZ9I4g6UvRCGQL07BkXNZSq0"
+			"/songs/Remote-Control/1.flac",
+			"/songs/Remote-Control/2.flac",
+			"/songs/Remote-Control/3.flac",
+			"/songs/Remote-Control/4.flac",
 		],
 	},
 	{
 		title: "Tell The Vision",
 		bpm: 144,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1mtwjHMfSgH0sFv0rR4vBtjG4jHhFlGX7",
-			"https://drive.google.com/uc?export=download&id=1moaOjfaGL3-rK7xZQf9mtR_zBKibE5DA",
-			"https://drive.google.com/uc?export=download&id=1msL2ocVUB7BEmEwGBRH4KMT4yarkE_a6",
-			"https://drive.google.com/uc?export=download&id=1mprRX3yqTG4nQhbDDIzAoF8H0Fohcjzi"
+			"/songs/Tell-The-Vision/1.flac",
+			"/songs/Tell-The-Vision/2.flac",
+			"/songs/Tell-The-Vision/3.flac",
+			"/songs/Tell-The-Vision/4.flac",
 		],
 	},
 	{
 		title: "Jonah",
 		bpm: 177,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1omIJQ1vdsQJ3sQxy3QD0yAQuH7p2Dhep",
-			"https://drive.google.com/uc?export=download&id=1oY6zaQy3CrvmkZtNsa-jxosdXSCUq7NO",
-			"https://drive.google.com/uc?export=download&id=1odq1rM7d2USTeB_rpIrzuS55RcwRR5Sb",
-			"https://drive.google.com/uc?export=download&id=1pSbGnRf1Szivye7Xb7p2Gmn7x8uj4hc-"
+			"/songs/Jonah/1.flac",
+			"/songs/Jonah/2.flac",
+			"/songs/Jonah/3.flac",
+			"/songs/Jonah/4.flac",
 		],
 	},
 	{
 		title: "Pure Souls",
 		bpm: 97,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1rB0J_exbQI2SfuDZr8-Rhl7JCxp9vnWN",
-			"https://drive.google.com/uc?export=download&id=1rV8JhJnrT44LXV6H5iqRPiNTeIIjLVe_",
-			"https://drive.google.com/uc?export=download&id=1rYU9dVEkAlgBtanI6bEUx0xVWUx14Wqo",
-			"https://drive.google.com/uc?export=download&id=1rWfkOPbN5oD2dbo8d0CkSxpIJfnzdiak"
+			"/songs/Pure-Souls/1.flac",
+			"/songs/Pure-Souls/2.flac",
+			"/songs/Pure-Souls/3.flac",
+			"/songs/Pure-Souls/4.flac",
 		],
 	},
 	{
 		title: "Ok Ok",
 		bpm: 108,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1jLiL24KNF3nr5zkdITCBBMUqwrv0hnbN",
-			"https://drive.google.com/uc?export=download&id=1ju-9ziC7bxaladQVaSGerTWi-uPGuG2a",
-			"https://drive.google.com/uc?export=download&id=1jLH_CDODPe1iB3UjyXcYe-TvtpWzGURM",
-			"https://drive.google.com/uc?export=download&id=1jyzWc5P0hRUEti5oVtA4qgboEINoq73Y"
+			"/songs/Ok-Ok/1.flac",
+			"/songs/Ok-Ok/2.flac",
+			"/songs/Ok-Ok/3.flac",
+			"/songs/Ok-Ok/4.flac",
 		],
 	},
 	{
 		title: "New Again",
 		bpm: 91,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1oT3mistVJkYQREAS6inoqz1cUTqY4Z-B",
-			"https://drive.google.com/uc?export=download&id=1oR0AmaUAQKbV7jgSyVL6O3YgeTr_8lbg",
-			"https://drive.google.com/uc?export=download&id=1oO2i-E31XYH9sINAUysF5kppaBtbIXtp",
-			"https://drive.google.com/uc?export=download&id=1oWKVYtK1BbSQ_V-tj6-g0i903lWZrWJn"
+			"/songs/New-Again/1.flac",
+			"/songs/New-Again/2.flac",
+			"/songs/New-Again/3.flac",
+			"/songs/New-Again/4.flac",
 		],
 	},
 	{
 		title: "Jesus Lord",
 		bpm: 106,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1rdkVlS5Ry9UAkb_zvv0kzFUPvkdfN7Uz",
-			"https://drive.google.com/uc?export=download&id=1rYQOUEWy9uRFLbQ1W2iDv0WUwz5l3mW2",
-			"https://drive.google.com/uc?export=download&id=1rdZQ773NYIQAJXo24K0wFxwUJmS0INsm",
-			"https://drive.google.com/uc?export=download&id=1rcH3YnIEhN5RbnTyy7EfD2obkleOmF1g"
+			"/songs/Jesus-Lord/1.flac",
+			"/songs/Jesus-Lord/2.flac",
+			"/songs/Jesus-Lord/3.flac",
+			"/songs/Jesus-Lord/4.flac",
 		],
 	},
 	{
 		title: "Ok Ok pt 2",
 		bpm: 108,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1l6RrTianO_SpnI427iOO_y75ZgWGK2rc",
-			"https://drive.google.com/uc?export=download&id=1l9hAKMfBgrN_q9ev-ZJ_xQnQqgP2OCDq",
-			"https://drive.google.com/uc?export=download&id=1lD6ZupvTg6XnPiSFPgRIxaw3milYDEfm",
-			"https://drive.google.com/uc?export=download&id=1l9CD-4ly-fpqfKq_ibo957hj2I89OEXQ"
+			"/songs/Ok-Ok-2/1.flac",
+			"/songs/Ok-Ok-2/2.flac",
+			"/songs/Ok-Ok-2/3.flac",
+			"/songs/Ok-Ok-2/4.flac",
 		],
 	},
 	{
 		title: "Junya pt. 2",
 		bpm: 148,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1rJPj6jnmra4_FBpXqk6Aa4BVsAzjx1VZ",
-			"https://drive.google.com/uc?export=download&id=1qsiTmvhtT5UvFQQyYMs7acnk5BMW7GZY",
-			"https://drive.google.com/uc?export=download&id=1rHdYtGDGmIkHIKulIOm2kvNIpaga23VE",
-			"https://drive.google.com/uc?export=download&id=1rHVMDrISXcLpHt2CxEdDEMvpmTSmgkUk"
+			"/songs/Junya-2/1.flac",
+			"/songs/Junya-2/2.flac",
+			"/songs/Junya-2/3.flac",
+			"/songs/Junya-2/4.flac",
 		],
 	},
 	{
 		title: "Jail pt. 2",
 		bpm: 110,
 		tracks: [
-			"https://drive.google.com/uc?export=download&id=1mkdZ9rVWoRUXWx8azC7UViVwgHzNOZov",
-			"https://drive.google.com/uc?export=download&id=1n8MS4wCPnQp_NRkFZGpEBODAotka6glM",
-			"https://drive.google.com/uc?export=download&id=1nMVHk1anIpQYcjbR0sCpqZPZhRczhjZZ",
-			"https://drive.google.com/uc?export=download&id=1nS74N7J5gISY6DHXGjLvsEypF41qeeh4"
+			"/songs/Jail-2/1.flac",
+			"/songs/Jail-2/2.flac",
+			"/songs/Jail-2/3.flac",
+			"/songs/Jail-2/4.flac",
 		],
 	},
 ]
-
-let oldPlaylist = [
-	[
-		// Ye - Pure Souls
-		"https://drive.google.com/uc?export=download&id=1rB0J_exbQI2SfuDZr8-Rhl7JCxp9vnWN",
-		"https://drive.google.com/uc?export=download&id=1rV8JhJnrT44LXV6H5iqRPiNTeIIjLVe_",
-		"https://drive.google.com/uc?export=download&id=1rYU9dVEkAlgBtanI6bEUx0xVWUx14Wqo", "https://drive.google.com/uc?export=download&id=1rWfkOPbN5oD2dbo8d0CkSxpIJfnzdiak" ],
-	[
-		// Ye - Happy
-		"https://lukew3.com/recolorbot/photos/Donda-2_Happy_1.mp3",
-		"https://lukew3.com/recolorbot/photos/Donda-2_Happy_2.mp3",
-		"https://lukew3.com/recolorbot/photos/Donda-2_Happy_3.mp3",
-		"https://lukew3.com/recolorbot/photos/Donda-2_Happy_4.mp3"
-	],
-	[
-		// Ye - We Made It Kid
-		"https://lukew3.com/recolorbot/photos/Donda_2_We%20Did%20It%20Kid_1.mp3",
-		"https://lukew3.com/recolorbot/photos/Donda_2_We%20Did%20It%20Kid_2.mp3",
-		"https://lukew3.com/recolorbot/photos/Donda_2_We%20Did%20It%20Kid_3.mp3",
-		"https://lukew3.com/recolorbot/photos/Donda_2_We%20Did%20It%20Kid_4.mp3"
-	],
-	[
-		// Ye - Follow God
-		"https://lukew3.com/recolorbot/photos/JIK_Follow_God_1.mp3",
-		"https://lukew3.com/recolorbot/photos/JIK_Follow_God_2.mp3",
-		"https://lukew3.com/recolorbot/photos/JIK_Follow_God_3.mp3",
-		"https://lukew3.com/recolorbot/photos/JIK_Follow_God_4.mp3"
-	]
-]
-
 
